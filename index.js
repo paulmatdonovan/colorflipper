bcc769354da4ac6ee14b4c350aa1e8b80feb76d7
Paul Donovan <paul.m.donovan@outlook.com>
2024-05-19 19:24:21 +1000

    more Js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-colors = ["green", "red", "blue", "rgba(133, 244, 123)"];
+const colors = ["green", "red", "blue", "rgba(133, 244, 123)"];
 const btn = document.getElementById("btn");
+console.log(btn);
 const color = document.querySelector('.color');
 
 btn.addEventListener('click', function () {
     const randomNumber = 2
-    document.body.style.backgroundColor = colors[randomNumber]
-})
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+    console.log(color);
+});
